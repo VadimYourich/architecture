c951d914aaa9f4a79135bf5de78d4d7967141612
vadim <89281779909@mail.ru>
2024-01-27 20:55:20 +0300

    hw_2

---
+++ HW_2/Abstractions/IGameItem.java
@@ -0,0 +1,6 @@
+package Abstractions;
+
+public interface IGameItem {
+    // Открывать и возвращать содержимое сундука
+    String open();
+}
---
+++ HW_2/Core/Core.java
@@ -0,0 +1,33 @@
+package Core;
+
+import Abstractions.ItemGenerator;
+import Rewards.Buratino.BuratinoGenerator;
+import Rewards.Gems.GemGenerator;
+import Rewards.Gold.GoldGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Core {
+	// Логика игры
+	public void run() {
+
+		// Создаем список генераторов лута
+		List<ItemGenerator> generatorList = new ArrayList<>();
+		generatorList.add(new GemGenerator());
+		generatorList.add(new GoldGenerator());
+		generatorList.add(new BuratinoGenerator());
+
+		
+		int min = 0;
+		int max = 2;
+
+		for (int i = 0; i < 10; i++) {
+			// Выбор одного из трех генераторов
+			int idx = ThreadLocalRandom.current().nextInt(min, max + 1);
+			ItemGenerator itemGenerator = generatorList.get(idx);
+			System.out.println(itemGenerator.openReward());
+		}
+	}
+}
